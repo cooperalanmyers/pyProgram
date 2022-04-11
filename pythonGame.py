ecb90dc6c004ae49efeea2ba3d4482fd56fae79a
Cooper Myers <60561014+cooperalanmyers@users.noreply.github.com>
2022-04-11 10:11:39 -0400

    Update pythonGame.py

--- pythonGame.py
+++ pythonGame.py
@@ -79,11 +79,10 @@ class Game():
                     if event.key == pygame.K_DOWN:
                         # Place New Ball
 
-
-           # If bat has hit the ball  
-           if ballrect.bottom >= batrect.top and ballrect.bottom <= batrect.bottom and ballrect.right >= batrect.left and ballrect.left <= batrect.right:
-              yspeed = -yspeed                
-              offset = ballrect.center[0] - batrect.center[0]
+                # If bat has hit the ball  
+                if ballrect.bottom >= batrect.top and ballrect.bottom <= batrect.bottom and ballrect.right >= batrect.left and ballrect.left <= batrect.right:
+                    yspeed = -yspeed                
+                    offset = ballrect.center[0] - batrect.center[0]
                   
                 
                 # offset > 0 means ball has hit RHS of bat                   
